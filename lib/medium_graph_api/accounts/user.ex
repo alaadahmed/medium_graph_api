062103e0bb908b481ac9dc5ddaf2c7b1ec7306ef
AlaaEldin Ahmed <alaadahmed@gmail.com>
2019-06-11 20:25:26 +0200

    implement user model validation

--- lib/medium_graph_api/accounts/user.ex
+++ lib/medium_graph_api/accounts/user.ex
@@ -5,9 +5,11 @@ defmodule MediumGraphApi.Accounts.User do
   schema "users" do
     field :first_name, :string
     field :last_name, :string
-    field :email, :string
+    field :email, :string, unique: true
     field :password_hash, :string
-    field :role, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+    field :role, :string, default: "user"
 
     timestamps()
   end
@@ -15,7 +17,17 @@ defmodule MediumGraphApi.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:first_name, :last_name, :email, :password_hash, :role])
-    |> validate_required([:first_name, :last_name, :email, :password_hash, :role])
+    |> cast(attrs, [:first_name, :last_name, :email, :password, :password_confirmation, :role])
+    |> validate_required([:first_name, :last_name, :email, :password, :password_confirmation, :role])
+    |> validate_format(:email, ~r/@/)
+    |> update_change(:email, &String.downcase(&1))
+    |> validate_length(:password, min: 6, max: 50)
+    |> validate_confirmation(:password)
+    |> unique_constraint(:email)
+    |> hash_password
+  end
+
+  defp hash_password(changeset) do
+    changeset
   end
 end
--- priv/repo/migrations/20190611174442_create_users.exs
+++ priv/repo/migrations/20190611174442_create_users.exs
@@ -11,5 +11,7 @@ defmodule MediumGraphApi.Repo.Migrations.CreateUsers do
 
       timestamps()
     end
+
+    create(unique_index(:users, [:email]))
   end
 end
