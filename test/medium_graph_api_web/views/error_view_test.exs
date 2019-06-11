c972732cee4d302bff4a1a61d9f7b112986b1263
AlaaEldin Ahmed <alaadahmed@gmail.com>
2019-06-11 18:57:09 +0200

    run formatter

--- test/medium_graph_api_web/views/error_view_test.exs
+++ test/medium_graph_api_web/views/error_view_test.exs
@@ -5,7 +5,9 @@ defmodule MediumGraphApiWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.json" do
-    assert render(MediumGraphApiWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+    assert render(MediumGraphApiWeb.ErrorView, "404.json", []) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500.json" do
