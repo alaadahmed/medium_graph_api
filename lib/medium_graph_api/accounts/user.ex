58f9513b410ecff01f1c44fb9ffb7202f66dcbb8
AlaaEldin Ahmed <alaadahmed@gmail.com>
2019-06-11 19:48:14 +0200

    scaffold user model files

---
+++ lib/medium_graph_api/accounts/user.ex
@@ -0,0 +1,21 @@
+defmodule MediumGraphApi.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :first_name, :string
+    field :last_name, :string
+    field :email, :string
+    field :password_hash, :string
+    field :role, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:first_name, :last_name, :email, :password_hash, :role])
+    |> validate_required([:first_name, :last_name, :email, :password_hash, :role])
+  end
+end
