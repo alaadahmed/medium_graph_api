43f4212248f9ee88a48756e3dd91136744e74a08
AlaaEldin Ahmed <alaadahmed@gmail.com>
2019-06-12 10:21:31 +0200

    add first query and mutation

---
+++ lib/medium_graph_api_web/resolvers/user_resolver.ex
@@ -0,0 +1,11 @@
+defmodule MediumGraphApiWeb.Resolvers.UserResolver do
+  alias MediumGraphApi.Accounts
+
+  def users(_, _, _) do
+    {:ok, Accounts.list_users()}
+  end
+
+  def register_user(_, %{input: input}, _) do
+    Accounts.create_user(input)
+  end
+end
